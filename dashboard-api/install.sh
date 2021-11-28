04921ec566c90d3a3f8a79c42f21574aca7d85e8
Bruno <brunocoelho1997@gmail.com>
2021-11-28 15:22:22 +0000

    Add logs

--- dashboard-api/install.sh
+++ dashboard-api/install.sh
@@ -1,14 +1,28 @@
 
 #stop container
+echo "\n\n\n\n\n"
+echo "----------------------------"
+echo "Stopping the container"
 docker rm -f "$(docker ps | grep "home-dashboard-api" | awk '{ print $1 }')"
 
 #delete old image
+echo "\n\n\n\n\n"
+echo "----------------------------"
+echo "Deleting the old image"
 docker rmi -f $(docker images --filter=reference="*dashboard-api-demo*" -q)
 
-cd ~/workspace/home-dashboard/dashboard-api/
 
+echo "\n\n\n\n\n"
+echo "----------------------------"
+echo "Maven - Building"
 mvn clean install
 
-docker build --tag home-dashboard-api-demo:v1.0 .
+echo "\n\n\n\n\n"
+echo "----------------------------"
+echo "Docker - building the new image"
+docker build --tag home-dashboard-api:v1.0 .
 
+echo "\n\n\n\n\n"
+echo "----------------------------"
+echo "Docker - running the new container"
 docker run -d -p 8080:8080 home-dashboard-api:v1.0

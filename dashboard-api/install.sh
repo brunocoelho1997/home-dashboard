5f82fc906ca8d19dc29efe90515793f7ab4386e3
Bruno <brunocoelho1997@gmail.com>
2021-11-28 15:14:15 +0000

    Add instalation shell script

---
+++ dashboard-api/install.sh
@@ -0,0 +1,14 @@
+
+#stop container
+docker rm -f $(docker ps | grep "home-dashboard-api" | awk '{ print $1 }')
+
+#delete old image
+docker rmi $(docker images --filter=reference="*dashboard-api-demo*" -q)
+
+cd ~/workspace/home-dashboard/dashboard-api/
+
+mvn clean install
+
+docker build --tag home-dashboard-api-demo:v1.0 .
+
+docker run -d -p 8080:8080 home-dashboard-api:v1.0

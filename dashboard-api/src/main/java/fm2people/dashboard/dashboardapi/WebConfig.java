0d3da07180517028fde44cf8ba5de4ac9a143205
Bruno <brunocoelho1997@gmail.com>
2021-12-02 20:28:25 +0000

    Enable cors

--- dashboard-api/src/main/java/fm2people/dashboard/dashboardapi/WebConfig.java
+++ dashboard-api/src/main/java/fm2people/dashboard/dashboardapi/WebConfig.java
@@ -1,2 +1,18 @@
-package fm2people.dashboard.dashboardapi;public class WebConfig {
+package fm2people.dashboard.dashboardapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
+
